2cd16d2c34f8d67193753c88ef0c90a0b4865131
Joe Hartley <josephlhartley@icloud.com>
2023-06-25 17:22:32 +0100

    Initial commit and implemented mmu

---
+++ MMU.cpp
@@ -0,0 +1,26 @@
+//
+// Created by hartl on 25/06/2023.
+//
+
+#include "MMU.h"
+
+MMU::MMU(uint8_t ConsoleMode) {
+    memset (memory, 0, sizeof(memory));
+
+    for (int i = 0x0000; i < 0x4000; i++) { // ROM + RAM + VRAM
+        MemoryMap[i] = memory + i;
+    }
+
+    if (!ConsoleMode) {
+        for (int i = 0x4000; i < 0x4400; i++) // Mirrored RAM
+            MemoryMap[i] = memory + i - 0x2000;
+    } else {
+        for (int i = 0x4000; i <= 0xFFFF; i++) // Unmirrored
+            MemoryMap[i] = memory + i;
+    }
+}
+
+void MMU::LoadInMemory(uint8_t *Buffer, uint16_t Address, int BufferSize) {
+    for (int i = 0; i < BufferSize; i++)
+        *(MemoryMap[Address + i]) = Buffer[i];
+}
---
+++ MMU.h
@@ -0,0 +1,38 @@
+//
+// Created by hartl on 25/06/2023.
+//
+
+#include <cstdint>
+#include <cstring>
+
+#ifndef I8080EMULATOR_MMU_H
+#define I8080EMULATOR_MMU_H
+
+// MMU Layout
+/*
+    ROM:			0x0000 - 0x1FFF
+    RAM:			0x2000 - 0x23FF
+    VRAM:			0x2400 - 0x3FFF
+    RAMMirror:	0x4000 - 0x43FF
+*/
+
+class MMU {
+public:
+    explicit MMU(uint8_t ConsoleMode);
+
+    // Addresses of different parts of memory
+    uint16_t ROMAddress = 0x0000;
+    uint16_t RAMAddress = 0x2000;
+    uint16_t VRAMAddress = 0x2400;
+    uint16_t RAMMirrorAddress = 0x4000;
+
+    // Useful pointer to VRAM
+    uint8_t* VRAM = memory + VRAMAddress;
+
+    uint8_t* MemoryMap[0x10000]{}; // 64K
+    void LoadInMemory(uint8_t* Buffer, uint16_t Address, int BufferSize);
+private:
+    uint8_t memory[0x10000]{};
+};
+
+#endif //I8080EMULATOR_MMU_H
